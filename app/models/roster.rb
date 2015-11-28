f689f99fabc1c7b8cfdaa8b36235f4bf704acc95
Zachary Levy <zacharyalevy@gmail.com>
2015-11-28 15:51:39 -0500

    team rosters

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -2,6 +2,7 @@ class Roster < ActiveRecord::Base
   has_many :plays1, :class_name => 'Play', :foreign_key => 'player1_id'
   has_many :plays2, :class_name => 'Play', :foreign_key => 'player1_id'
   has_many :plays3, :class_name => 'Play', :foreign_key => 'player1_id'
+  belongs_to :team, :class_name => 'Team', :foreign_key => 'team_id'
 
   def plays
     plays1 + plays2 + plays3
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,7 @@ class Team < ActiveRecord::Base
   has_many :home_games, :class_name => "Game", :foreign_key => :home_id
   has_many :away_plays, through: :away_games, :source => :plays
   has_many :home_plays, through: :home_games, :source => :plays
+  has_many :rosters, :class_name => "Roster", :foreign_key => "team_id"
 
   def plays
     away_plays + home_plays
