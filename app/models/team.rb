362cafce05f510817c2c01f99fa08139101c22b0
Zachary Levy <zacharyalevy@gmail.com>
2015-11-28 15:48:57 -0500

    team plays

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,12 @@
 class Team < ActiveRecord::Base
   has_many :away_games, :class_name => "Game", :foreign_key => :away_id
   has_many :home_games, :class_name => "Game", :foreign_key => :home_id
+  has_many :away_plays, through: :away_games, :source => :plays
+  has_many :home_plays, through: :home_games, :source => :plays
+
+  def plays
+    away_plays + home_plays
+  end
 
   def games
     away_games + home_games
